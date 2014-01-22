3c3f6032c9466a5a654ca5e5701d7a006d264556
drjekyll3 <vidya3v@gmail.com>
2014-01-21 22:47:56 -0800

    First commit.

---
+++ src/Euler2.java
@@ -0,0 +1,31 @@
+public class Euler2 {
+	/***
+	 * http://projecteuler.net/problem=1
+	 * @param n
+	 * @return sum of all the multiples of 3 or 5 below 1000.
+	 */
+	public static int sumMultiplesOf3and5(int n) {
+		return sumMultiplesOf3and5(n, 1);
+	}
+	
+	/***
+	 * Helper function
+	 * @param n
+	 * @param current
+	 * @return sum of all the multiples of 3 or 5 below 1000.
+	 */
+	public static int sumMultiplesOf3and5(int n, int current) {
+		if (current == n) {
+			return 0;
+		}
+		
+		if (current % 3 == 0 || current % 5 == 0) {
+			return current + sumMultiplesOf3and5(n, current + 1);
+		}
+		return sumMultiplesOf3and5(n, current + 1);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Euler2.sumMultiplesOf3and5(1000));
+	}
+}
